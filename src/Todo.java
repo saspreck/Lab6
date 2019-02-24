805be9aea1494ae782ab8d673f0c26a29a80ac7c
Skyler Sprecker <saspreck@gmail.com>
2019-02-23 18:48:56 -0600

    Finished shape class

---
+++ src/Todo.java
@@ -0,0 +1,20 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-23
+OU ID_______________: 113384038
+OU EMAIL____________: saspreck@ou.edu
+
+Tasks:
+1. complete Shape compareTo method
+2. complete Shape toString method
+3.
+
+Estimated Time (minutes):
+1. 10
+2. 5
+
+Actual Time (minutes):
+1. 10
+2. 7
+
+*/}
